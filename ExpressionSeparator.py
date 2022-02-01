c35fd458a720dc3c5ca8c3682bdb80a9f998c7a1
DzmAstr <ostrowskidymitr@gmail.com>
2022-02-01 04:11:55 +0100

    Parallel Calculator algorithm finished.  Exspression Separator fixed.

--- ExpressionSeparator.py
+++ ExpressionSeparator.py
@@ -1,15 +1,20 @@
+from distutils.util import subst_vars
 from typing import Dict, List
-from attr import define
 
 import numpy as np
 
 
 
 class Expression:
-    
-    def __init__(self, operation_txt:str, priority = 0) -> None:
+
+    def __init__(self, operation_txt, priority = 0) -> None:
         self.operation_txt = operation_txt
         self.priority = priority
+        self.larg, self.operation, self.rarg = operation_txt
+        self.id = 0
+
+    def set_id(self, id):
+        self.id = id
 
     def rise_priority(self) -> None:
         self.priority += 1
@@ -18,8 +23,6 @@ class Expression:
         return f"Expression: {self.operation_txt}, priority = {self.priority}"
 
 
-    
-    
 
 class ExpressionSeparator:
 
@@ -32,18 +35,21 @@ class ExpressionSeparator:
         self.numbers = list('1234567890')
         
 
-
     def add_expression_to_list(self, string:str, priority = 0):
+        string = [ elem if type(elem) != type([]) else elem[0] for elem in string ]
         self.exp_list.append(Expression(string, priority))
+        self.exp_list[-1].set_id(len(self.exp_list)-1)
 
     
     def convert_string_to_exprs(self, string:str) -> List[Expression]:
         self.exp_full_str = list(string)
         self.exp_full_str = self.fix_separated_numbers(self.exp_full_str)
+        self.exp_full_str = self.fix_subtraction_operations(self.exp_full_str)
         clean_list = self.extract_parenthesis(self.exp_full_str)
         self.rise_all_priorities()
         self.define_exp_for_prepared_data(clean_list)
-        self.fix_complex_elements_in_exp_list()
+        return self.exp_list
+        # self.fix_complex_elements_in_exp_list()
 
 
     def define_exp_for_prepared_data(self, arr):
@@ -81,26 +87,26 @@ class ExpressionSeparator:
 
     def define_exp_operation_for_math_eq(self, equation):
 
-        equation =  np.array(equation)
+        equation =  np.array(equation, dtype=object)
         for operator in self.supported:
             if any(np.isin(equation, operator)) :
                 operator_index = np.argmax(equation == operator)
                 if operator_index > 0:
                     new_exp = equation[operator_index-1 : operator_index+2].copy()
-                    
+                    np.array([*zip(*equation)]).ravel()
                     self.add_expression_to_list(new_exp,1)
                     equation[operator_index+1] = f'self.exp_list[{self.get_free_exp_cell_idx()}]'
                     equation[operator_index-1 : operator_index+1] = ''
                     equation = equation[equation.nonzero()]
                     self.rise_all_priorities()
-                    return equation.tolist()
+                    return equation.tolist() # [*equation] #.tolist()
           
  
     def rise_all_priorities(self):
         for e in self.exp_list: e.rise_priority()
 
     def get_free_exp_cell_idx(self):
-            return len(self.exp_list)
+            return len(self.exp_list)-1
 
     def fix_separated_numbers(self, arr):
         arr = np.array(arr)
@@ -125,52 +131,67 @@ class ExpressionSeparator:
                 _last_numerical_elem_cnt = 0
         return _arr
 
+    def fix_subtraction_operations(self, arr):
+        subst_char = '-'
+        out = []
+        for idx, charr in enumerate(arr):
+            if(charr == subst_char): 
+                out.append('+')
+                out.append('-1')
+                out.append('*')
+            else:
+                out.append(charr)
+        return out
 
 
     def extract_parenthesis(self, string:str):
-
-        
         
         arr_no_parenth = []
-        exp = []
+        
         supported = list('*/+-')
         parenthesis = list('()')
 
         patenth_tmp = []
         parenth_idx = []
         right_parenth_cnt = 0
-
+        left_parenth_cnt = 0
+        '(1-1+(6+7))*2+3*(1-3+4)+10/2'
         for idx, char in enumerate(string[:]):
                 if char == '(' :
                     parenth_idx.append(idx)
                     right_parenth_cnt +=1
                 elif char == ')' :
                     parenth_idx.append(idx)
+                    left_parenth_cnt += 1
+                    if right_parenth_cnt != left_parenth_cnt:
+                        continue
                     if len(parenth_idx) == 2:
                         parenthesis_exp = string[parenth_idx[0]:(parenth_idx[-1]+1)]
                         parenthesis_exp =  parenthesis_exp[1:-1]#parenthesis_exp.replace("(",'', 1).replace(")",'', 1)
-                        arr_no_parenth.append(f'self.exp_list[{self.get_free_exp_cell_idx()}]')
-                        self.add_expression_to_list(parenthesis_exp,1)
-                        exp.append(parenthesis_exp)
+                        exp_tmp = parenthesis_exp
+                        while(len(exp_tmp)>1):
+                            exp_tmp = self.define_exp_operation_for_math_eq(exp_tmp)
+                        
+                        arr_no_parenth.append(exp_tmp) #(f'self.exp_list[{self.get_free_exp_cell_idx()}]')
+                        # self.add_expression_to_list(parenthesis_exp,1)
+                        
                         parenth_idx = []
+                        left_parenth_cnt = 0
                         right_parenth_cnt = 0
                     elif len(parenth_idx)%2 == 0 and char == ')':
                         parenthesis_exp = string[parenth_idx[0]:(parenth_idx[-1]+1)]
                         parenthesis_exp = parenthesis_exp[1:-1]
                         _arr = self.extract_parenthesis(parenthesis_exp)
+                        while(len(_arr)>1):
+                            _arr = self.define_exp_operation_for_math_eq(_arr)
                         arr_no_parenth.append(_arr)
                         parenth_idx = []
+                        left_parenth_cnt = 0
                         right_parenth_cnt = 0
-                    # else:
-                    #     pass
-                
-                # elif right_parenth_cnt >1:
-                #     right_parenth_cnt -=1                
 
                 elif len(parenth_idx) == 0:
                     arr_no_parenth.append(char)
-            
-        # exp = [str.replace("(",'', 1).replace(")",'', 1) for str in exp] 
+  
         return arr_no_parenth
 
 
--- ParallelCalculator.py
+++ ParallelCalculator.py
@@ -1,14 +1,69 @@
+import ray
+import numpy as np
+
+import time 
 
 
+from ExpressionSeparator import ExpressionSeparator , Expression
 
+ray.init()
 
+@ray.remote
+def send_result(res):
+    pass
 
-
-
-
+@ray.remote
+def count_expression(larg, rarg, operation, operation_id = 0):   
+    tmp = 0
+    if type(larg) == type('') and type(rarg) == type(''):
+        tmp = eval(larg + operation + rarg)
+    elif type(rarg) == type('') and type(larg) != type('') :
+        tmp = eval(str(larg) + operation + rarg)
+    elif type(rarg) != type('') and type(larg) == type('') :
+        tmp = eval(larg + operation + str(rarg))
+    elif type(rarg) != type('') and type(larg) != type('') :
+        tmp = eval(str(larg) + operation + str(rarg))
+    time.sleep(3)
+    print(f'operation = {larg , operation , rarg} = {tmp}, id = {operation_id}, time = {time.time()}')
+    return tmp
 
 
 class ParallelCalculator:
     
     def __init__(self) -> None:
-        pass
+        self.exp_list = []
+        self.exp_result = []
+        self.separator = ExpressionSeparator()
+
+    def delegate_operations_exec_from_exp_list(self):
+        # exp_sorted = sorted(self.exp_list, key=lambda ex: ex.priority, reverse=True)
+
+        operations = []
+        tmp_arr = []
+        for idx, exp in enumerate(self.exp_list):
+            # operations.append( "".join(exp.operation_txt))
+            
+            larg = exp.larg
+            rarg = exp.rarg
+            operation = exp.operation
+            
+            if 'exp' in exp.larg:                
+                tmp = eval(exp.larg)
+                larg = self.exp_result[tmp.id]                
+
+            if 'exp' in exp.rarg:
+                tmp = eval(exp.rarg)
+                rarg = self.exp_result[tmp.id]    
+            
+
+            self.exp_result[idx] = count_expression.remote(larg, rarg, operation, exp.id) #.remote
+
+        while(1):
+            pass
+
+
+        
+    def count_value_from_string(self, string):
+        self.exp_list = self.separator.convert_string_to_exprs(string)
+        self.exp_result = np.zeros(len(self.exp_list), dtype= object)
+        self.delegate_operations_exec_from_exp_list()
